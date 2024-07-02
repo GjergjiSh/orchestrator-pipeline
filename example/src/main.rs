cd2179cb2745f6f8feb321b6d9ceeb1804151665
gshkurti <gjergji.shkurti@vector.com>
2024-07-02 16:13:24 +0200

    add sample module configuration reading

--- example/src/main.rs
+++ example/src/main.rs
@@ -2,9 +2,14 @@
 
 extern crate libloading;
 use orchestrator_pipeline::prelude::*;
+use std::path::Path;
 
 fn main() {
-    let path = std::path::Path::new(r"C:\Users\gshkurti\Repos\orchestrator-pipeline\target\debug\debug_module.dll");
+    let path = Path::new(r"C:\Users\gshkurti\Repos\orchestrator-pipeline\target\debug\debug_module.dll");
+    let cfg_path = Path::new(r"C:\Users\gshkurti\Repos\orchestrator-pipeline\sample-config.yaml");
+    let module_config = ModuleConfig::new(cfg_path);
+    let modules = module_config.modules();
+    dbg!(modules);
     let module = load_module(path);
     let mut orchestrator = Orchestrator::new();
     orchestrator.register_module(module);
--- orchestrator-pipeline/src/module/mod.rs
+++ orchestrator-pipeline/src/module/mod.rs
@@ -1,10 +1,46 @@
 use crate::shared_data::SharedData;
+use std::{collections::HashMap, io::Read};
 use serde::Deserialize;
+
+#[derive(Deserialize, Clone)]
+struct IndividualModuleConfig {
+    name: String,
+    parameters: HashMap<String, String>,
+}
 
 #[derive(Deserialize)]
 pub struct ModuleConfig {
-    name: String,
-    parameters: std::collections::HashMap<String, String>,
+    module_root: String,
+    modules: Vec<IndividualModuleConfig>,
+}
+
+impl ModuleConfig {
+    pub fn new(path: &std::path::Path) -> Self {
+        if !path.exists() {
+            panic!("Module configuration not found at: {:?}", path)
+        }
+
+        let mut file = std::fs::File::open(path)
+            .unwrap_or_else(|e| panic!(
+                "Failed to read configuration file. Error: {}", e)
+            );
+
+        let mut config_content = String::new();
+        file.read_to_string(&mut config_content);
+
+        let config: ModuleConfig = serde_yaml::from_str(&config_content)
+            .unwrap_or_else(|e| panic!(
+            "Failed to parse configuration content. Error: {}", e)
+        );
+
+        config
+    }
+
+    pub fn modules(&self) -> HashMap<String, HashMap<String, String>> {
+        self.modules.clone().into_iter()
+        .map(|module| (module.name, module.parameters))
+        .collect()
+    }
 }
 
 pub trait Module {
