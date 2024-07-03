a7a5d68796c5864c8af65134e2f0b9f29891eaff
gshkurti <gjergji.shkurti@vector.com>
2024-07-03 15:25:08 +0200

    add motors struct to shared data

--- orchestrator-pipeline/src/shared_data.rs
+++ orchestrator-pipeline/src/shared_data.rs
@@ -1,12 +1,27 @@
 #[derive(Debug)]
+pub struct Motors {
+    left: i32,
+    right: i32,
+}
+
+#[derive(Debug)]
 pub struct SharedData {
-    foo: i32
+    motors: Motors
+}
+
+impl Motors {
+    pub fn new() -> Self {
+        Motors {
+            left: 0,
+            right: 0
+        }
+    }
 }
 
 impl SharedData {
     pub fn new() -> Self {
         SharedData {
-            foo: 0
+            motors: Motors::new()
         }
     }
 }
